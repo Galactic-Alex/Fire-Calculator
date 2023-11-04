0cdb2f695f97ceb697198cb4968bf12e2fc3a9b8
Galactic Alex <Lehahahaigaleksus@mail.ru>
2023-11-04 19:06:21 +0300

    Finished FireCalculator

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import calculator.Calculator;
+import userInterface.YearInvalidException;
+import userInterface.UserInterface;
+import userInterface.UserInterfaceDelegate;
+
+import java.util.Scanner;
+
+public class Main implements UserInterfaceDelegate {
+    private final Calculator calculator = new Calculator();
+    public static void main(String[] args) throws YearInvalidException {
+        Main main = new Main();
+        main.userInterfaceSetup();
+    }
+
+    @Override
+    public double userInterfaceDidScan(Integer userInput) {
+        return calculator.calculateWithdrawalPercentage(userInput);
+    }
+
+    private void userInterfaceSetup() throws YearInvalidException {
+        Scanner scanner = new Scanner(System.in);
+        UserInterface userInterface = new UserInterface(scanner);
+        userInterface.delegate = this;
+        userInterface.startInterface();
+
     }
 }
